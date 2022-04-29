e12df1bcc69e6c47176c79ee462c0107cae50929
cwalinskid1 <cwalinskid@southington.org>
2022-04-29 16:29:17 -0400

    new lessons

---
+++ Lesson2-IntroToStatsInPython/IntroToStatsandProb.py
@@ -0,0 +1,14 @@
+#import libraries
+import numpy as np
+import pandas as pd
+import random
+import matplotlib.pyplot as plt
+
+#create a list of random numbers
+sample = [ random.randint(0,10)for _ in range(30)]
+print(f"Sample: {sample}")
+print(f"Mean: {np.mean(sample)}")
+print(f"Variance: {np.var(sample)}")
+#plot a histogram of the sample
+plt.hist(sample)
+plt.show()
